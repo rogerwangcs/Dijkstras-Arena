52a193b944091489c32dfc77ff85b59f11b8ae15
Roger Wang <rogerwangcs@gmail.com>
2019-12-01 02:57:07 -0500

    add client

---
+++ client/build/precache-manifest.6f81a93c1dafd296b42918ade3028034.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d222dcd6c8d26039eadb5461a6817f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "323b058804a4ca4a14a5",
+    "url": "/static/css/main.fb845edd.chunk.css"
+  },
+  {
+    "revision": "896ba1f466c281133d9a",
+    "url": "/static/js/2.f744c7d8.chunk.js"
+  },
+  {
+    "revision": "323b058804a4ca4a14a5",
+    "url": "/static/js/main.7edee92d.chunk.js"
+  },
+  {
+    "revision": "a10e04f2fd92747200c9",
+    "url": "/static/js/runtime-main.690eb59f.js"
+  }
+]);
